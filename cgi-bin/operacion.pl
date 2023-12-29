91ec43c196fa1eb78caf91914c3449e35181dc9f
JorgeLearning <mamannihuarsaya4b@gmail.com>
2023-12-29 18:41:09 +0000

    Bien, ahora empezamos con las verificaciones. Se implemento la subrutina operate que se va a comportar como la subrutina init que llama a las demas subrutinas. Por el momento la subrutina verify se encarga de validar la expresion: solo se permiten numeros, +, -, *, /, (, ) y espacios en blanco. Ademas el primer caracter solo puede ser un numero, +, - y (, el final caracter solo puede ser un numero y )

--- cgi-bin/operacion.pl
+++ cgi-bin/operacion.pl
@@ -4,7 +4,29 @@ use strict;
 use CGI;
 
 my $q = CGI->new;
-my $operation = $q->param("operation");
+my $operation = $q->param("expression");
+
+
+sub operate {
+  # Eliminamos los espacios en blanco
+  my $expression = $_[0];
+  $expression =~ s/\s//g;
+
+  # Validamos la expresión
+  if(!verify($expression)) {
+    return "Expresion no valida\n";
+  }
+
+  return "Expresion valida\n";
+}
+
+sub verify {
+  my $expression = $_[0];
+  if($expression =~ /^[0-9+\-\(\s]+[0-9\s\)]$/) { 
+    return 1;  
+  }
+}
 
 print $q->header("text/html");
-print $operation;
+print operate($operation)."\n";
+
