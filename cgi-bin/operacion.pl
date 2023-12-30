bb08ddab46b6eba1b2d9e37e06703e61d35bb612
JorgeLearning <mamannihuarsaya4b@gmail.com>
2023-12-30 15:39:49 +0000

    Tratamos de establecer las condiones para los operadores sin embargo se tenido complicaciones con la codificacion del parametro, al probarlo por la terminal el carcate + se interpreta como espacio en blanco y usando el formulario llega con otros caracteres al CGI, como %2B para +

--- cgi-bin/operacion.pl
+++ cgi-bin/operacion.pl
@@ -6,27 +6,37 @@ use CGI;
 my $q = CGI->new;
 my $operation = $q->param("expression");
 
-
 sub operate {
   # Eliminamos los espacios en blanco
   my $expression = $_[0];
+  print "$expression\n";
   $expression =~ s/\s//g;
 
   # Validamos la expresión
-  if(!verify($expression)) {
+  if(!validateOverall($expression)) {
     return "Expresion no valida\n";
   }
+
+  validateOperators($expression);
 
   return "Expresion valida\n";
 }
 
-sub verify {
+sub validateOverall {
   my $expression = $_[0];
-  if($expression =~ /^[0-9+\-\(\s]+[0-9\s\)]$/) { 
+  if(($expression =~ /^[0-9+\-\*\/\(\)]+$/) && ($expression =~ /^[0-9-\(]/) && ($expression =~ /[0-9\)]$/)) {
     return 1;  
   }
 }
 
-print $q->header("text/html");
-print operate($operation)."\n";
+sub validateOperators {
+  my $expression = $_[0];
+  print "$expression\n\n";
+  my @segments = $expression =~ /\d+[+\-\*\/]{1,2}\d+/g; 
+  foreach my $segment (@segments) {
+    print "$segment\n";
+  }
+}
 
+print $q->header("text/html; charset=UTF-8");
+print "\n".operate($operation)."\n";
