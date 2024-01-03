8a5999042ef42ba361322622a5c23f59af8bb32e
JorgeLearning <mamannihuarsaya4b@gmail.com>
2024-01-03 08:06:46 +0000

    Agregamos la subrutina generateRegExp que se encarga de dividir en n columnas (que vendrian a ser los items) la linea analizada. Entonces para generalizar el numero de columnas que contiene cada linea, entra en accion la subrutina readHeader que nos deberia devolver la cantidad de vueltas necesarias que el ciclo da para dividir y dividir la cadena hasta ya no encontrar mas caracteres | (tuberia)

--- consult_licensed_universities/cgi-bin/consult.pl
+++ consult_licensed_universities/cgi-bin/consult.pl
@@ -7,15 +7,32 @@ open(IN, "Programas_de_Universidades.csv") or die("Error: No se pudo leer el arc
 my @arr = <IN>;
 close(IN);
 
-my $line = $arr[0];
-# La expresion regular captura un flujo de caracteres hasta encontrar el "|". 
-while($line =~ /^([^\|]+)\|(.+)/) {
-  # Una vez que se encuentra "|" la cadena se separa en dos partes
-  print "$1\n"; 
-  # Actualizamos $line para que la subcadena se vuelva a analizar
-  $line = $2;
+sub readHeader {
+  my $line = $arr[0];
+  # La expresion regular captura un flujo de caracteres hasta encontrar el "|". 
+  while($line =~ /^([^\|]+)\|(.+)/) {
+    # Una vez que se encuentra "|" la cadena se separa en dos partes
+    print "$1\n"; 
+    # Actualizamos $line para que la subcadena se vuelva a analizar
+    $line = $2;
+  }
+  # Como para la ultima $line no se encontrara "|", el while para y no se 
+  # imprime el ultimo item
+  print "$line\n";
 }
-# Como para la ultima $line no se encontrara "|", el while para y no se 
-# imprime el ultimo item
-print "$line\n";
+
+sub generateRegExp {
+  my $n = $_[0];
+  my $exp = "^";
+  # Generamos la expresion regular que divide en n grupos a la linea analizada
+  for(my $i = 0; $i < $n - 1; $i++) {
+    $exp .= "([^\|]+)\|";
+  }
+  $exp .= "([^\|]+)";
+  return $exp;
+}
+
+# En este caso probamos con 4 divisiones, de hecho deberiamos saber el numero de columnas
+# de cada linea
+print generateRegExp(4)."\n";
 
