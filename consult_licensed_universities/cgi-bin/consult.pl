9a8e462c594d179b0bae785fb77b78c62cd6382b
JorgeLearning <mamannihuarsaya4b@gmail.com>
2024-01-03 07:17:52 +0000

    Para separar los items para cada dato, hemos utilizado una expresion regular que se encargue de dividir la cadena en dos partes cada vez que encuentre

--- consult_licensed_universities/cgi-bin/consult.pl
+++ consult_licensed_universities/cgi-bin/consult.pl
@@ -7,6 +7,15 @@ open(IN, "Programas_de_Universidades.csv") or die("Error: No se pudo leer el arc
 my @arr = <IN>;
 close(IN);
 
-# Verificamos la correcta lectura
 my $line = $arr[0];
+# La expresion regular captura un flujo de caracteres hasta encontrar el "|". 
+while($line =~ /^([^\|]+)\|(.+)/) {
+  # Una vez que se encuentra "|" la cadena se separa en dos partes
+  print "$1\n"; 
+  # Actualizamos $line para que la subcadena se vuelva a analizar
+  $line = $2;
+}
+# Como para la ultima $line no se encontrara "|", el while para y no se 
+# imprime el ultimo item
 print "$line\n";
+
