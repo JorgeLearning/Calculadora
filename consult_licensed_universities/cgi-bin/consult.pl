ae358691c8cb1e2b44f4441617d80cf63bc558b2
JorgeLearning <mamannihuarsaya4b@gmail.com>
2024-01-03 06:21:05 +0000

    Implementamos la logica para leer el archivo, nuestro objetivo es que un arreglo se comporte como un buffer en el cual se almacena el archivo. Cada posicion del arreglo representa cada linea del archivo

--- consult_licensed_universities/cgi-bin/consult.pl
+++ consult_licensed_universities/cgi-bin/consult.pl
@@ -2,9 +2,11 @@
 use strict;
 use warnings;
 
+# Leemos el archivo en un arreglo
 open(IN, "Programas_de_Universidades.csv") or die("Error: No se pudo leer el archivo");
 my @arr = <IN>;
 close(IN);
 
+# Verificamos la correcta lectura
 my $line = $arr[0];
 print "$line\n";
