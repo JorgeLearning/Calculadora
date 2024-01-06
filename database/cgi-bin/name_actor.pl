e3883173fa9cf382f1f055c2e638ce7d2ad2d701
Jorge <mamannihuarsaya4b@gmail.com>
2024-01-06 23:09:03 +0000

    Realizamos la solicitud de la base de datos e imprimimos, recordemos que la funcion fetchrow_array devuelve la fila especifica, que despues la estamos almacenando en un arreglo donde cada posicion contiene a cada campo

--- database/cgi-bin/name_actor.pl
+++ database/cgi-bin/name_actor.pl
@@ -15,10 +15,11 @@ my $dsn = "DBI:MariaDB:database=pweb1;host=$ip_address";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");;
 
 # Realizamos una insercion
-my $sth = $dbh->prepare("INSERT INTO Actor(ActorID, Name) VALUES (?,?)");
-$sth->execute(5, "Speed Racer");
+my $id = 5;
+my $sth = $dbh->prepare("SELECT * FROM Actor WHERE ActorID=?");
+$sth->execute($id);
+while(my @row = $sth->fetchrow_array) {
+  print "@row\n";
+}
 $sth->finish;
-print "Insercion terminada.\n";
-
-# Nos desconectamos del SGBD
 $dbh->disconnect;
