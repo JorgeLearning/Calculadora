64f65b97104de30d4586fc23f2402c4f8ae85a19
Jorge <mamannihuarsaya4b@gmail.com>
2024-01-07 05:34:10 +0000

    Bien, despues de las pruebas por la terminal, agregamos el modulo CGI para poder, en este caso, generar codigo html. Usaremos una tabla para ordenar los datos de respuesta

--- database/cgi-bin/name_actor.pl
+++ database/cgi-bin/name_actor.pl
@@ -2,6 +2,26 @@
 use strict;
 use warnings;
 use DBI;
+use CGI;
+
+my $q = CGI->new;
+print $q->header("text/html", "charset=UTF-8");
+print<<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <title>Resultado Universidades</title>
+</head>
+<body>
+  <h2>Actriz</h2>
+  <table border="1">
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+    </tr>
+HTML
 
 # Establenciendo el IP del servidor
 my $ip_address = qx(hostname -I);
@@ -19,7 +39,9 @@ my $id = 5;
 my $sth = $dbh->prepare("SELECT * FROM Actor WHERE ActorID=?");
 $sth->execute($id);
 while(my @row = $sth->fetchrow_array) {
-  print "@row\n";
+  print "<tr><td>$row[0]</td>\n<td>$row[1]</td>\n";
 }
 $sth->finish;
 $dbh->disconnect;
+
+print "</table></body></html>\n";
